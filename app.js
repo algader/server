4a711b2b465a5fa01bc53333abb42c8fc134bd98
alsukkari.mohammad <alsukkari.mohammad@icloud.com>
2024-01-07 11:39:38 +0100

    add morgan module

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
-var expressValidator = require('express-validator');
 const express = require('express');
 const router = require('./routes');
 const cors = require('cors');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require ('body-parser');
 const db = require('./models/database');
 const models = require('./models');
@@ -20,7 +19,7 @@ const app = express();
         
 app.use(cors());
 
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 
 
